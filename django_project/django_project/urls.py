f1d3aad3b880ebf5583744fdf1ce683b144ddd4e
xinglin2016 <lixinglin0829@gmail.com>
2021-06-04 15:10:05 -0400

    modify settings and migration

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# admin.autodiscover()
+# url(r'^admin/', include(admin.site.urls)),
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
