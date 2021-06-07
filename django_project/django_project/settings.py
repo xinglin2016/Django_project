310dbbfad65b01338010c744468c8a8d5ac71e58
xinglin2016 <lixinglin0829@gmail.com>
2021-06-06 22:23:19 -0400

    modify ALLOWED_HOSTS

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e-x^^l!(qmrp-thtmq$3+vlt4qj)+((luju8ui^4t2_3$qu$s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 # admin.autodiscover()
 # url(r'^admin/', include(admin.site.urls)),
 
 urlpatterns = [
+    path('myapp/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
